47b6b2ef7c57ce607ec09b19caaf1192d5afafc9
Rita Verstraeten <rita.verstraeten@ugent.be>
2024-09-26 13:48:31 +0200

    Einstein summation fixed

--- src/DENV_model/DENV_models_pySODM.py
+++ src/DENV_model/DENV_models_pySODM.py
@@ -125,6 +125,80 @@ class JumpProcess_SEIRH_BetaPerAge_model(JumpProcess):
         
         return S_new, E1_new, E2_new, I1_new, I2_new, R1_new, R2_new, H1_new, H2_new, R12_new   
 
+################################
+# the TEMPORAL SEIR2 model + SF
+################################
+
+class JumpProcess_SEIR2_SeasonalForcing(JumpProcess):
+    """
+    Stochastic "real" SEIR2 model for DENV with 2 serotypes, no age-groups and + seasonal forcing.
+
+    Beta_0 represents the baseline beta 
+    Beta_1 represents the additional infectivity due to seasonal forcing, which is dependent on time 
+    The ph parameter represents the phase of the cos forcing curve, this is adjusted shift the curve to fit the DENV temporal patterns in Cuba
+    All the E, I, R compartments are available twice in the model to represent both serotypes
+    The S and R12 are independent of serotype
+    """
+    
+    states = ['S','S1', 'S2', 'E1', 'E2', 'E12', 'E21', 'I1', 'I2', 'I12', 'I21', 'R1', 'R2', 'R']
+    parameters = ['alpha', 'beta_0', 'beta_1', 'sigma', 'gamma', 'psi', 'ph'] # the beta_1 parameter is the same over all age-groups seeing as seasonality affects everyone the same
+
+    @staticmethod
+    def compute_rates(t, S,S1, S2, E1, E2, E12, E21, I1, I2, I12, I21, R1, R2, R, beta_0, beta_1, alpha, sigma, gamma, psi, ph):
+        
+        # Calculate total population
+        T = S+E1+I1+R1+S1+E12+I12+E2+I2+R2+S2+E21+I21+R
+
+        # calculate the Beta_t
+        #beta_t = beta_0 * (1 - beta_1 * np.cos(2*np.pi *(t/365) + ph)) # this should result in 4 values per timepoint (one for each age-group)
+        beta_t = beta_0 * (1 - beta_1 * np.sin(2*np.pi *(t/365) + ph))
+
+        # Compute rates per model state
+        rates = {
+
+            'S': [beta_t*((I1+ psi*I21)/T), beta_t*((I2+psi*I12)/T)], # I think this multiplication with np.ones(T.shape is for the age-groups) but maybe this is not needed seeing as I1 and H1 will already have the correct shapes
+            # does nothing change in the rate calculation for S? Does the stratified parameter automatically follow the age-groups?
+            'S1': [beta_t*((I2+psi*I12)/T),],
+            'S2': [beta_t*((I1+ psi*I21)/T),],
+
+            'E1': [1/sigma*np.ones(T.shape),],
+            'E2': [1/sigma*np.ones(T.shape),],
+            'E12': [1/sigma*np.ones(T.shape),],
+            'E21': [1/sigma*np.ones(T.shape),], 
+
+            'I1': [1/gamma*np.ones(T.shape),],
+            'I2': [1/gamma*np.ones(T.shape),],
+            'I12': [1/gamma*np.ones(T.shape),],
+            'I21': [1/gamma*np.ones(T.shape),],
+
+            'R1': [1/alpha*np.ones(T.shape),],
+            'R2': [1/alpha*np.ones(T.shape),]
+            }
+        
+        return rates
+    
+    @staticmethod
+    def apply_transitionings(t, tau, transitionings, S,S1, S2, E1, E2, E12, E21, I1, I2, I12, I21, R1, R2, R, beta_0, beta_1, alpha, sigma, gamma, psi, ph):
+
+        S_new  = S - transitionings['S'][0] - transitionings['S'][1]
+        E1_new = E1 + transitionings['S'][0] - transitionings['E1'][0]
+        E2_new = E2 + transitionings['S'][1] - transitionings['E2'][0]
+        I1_new = I1 + transitionings['E1'][0] - transitionings['I1'][0]
+        I2_new = I2 + transitionings['E2'][0] - transitionings['I2'][0]
+        R1_new = R1 + transitionings['I1'][0] - transitionings['R1'][0]
+        R2_new = R2 + transitionings['I2'][0] - transitionings['R2'][0]
+
+        S1_new = S1 + transitionings['R1'][0] - transitionings['S1'][0]
+        S2_new = S2 + transitionings['R2'][0] - transitionings['S2'][0]       
+        E12_new = E12 + transitionings['S1'][0] - transitionings['E12'][0]
+        E21_new = E21 + transitionings['S2'][0] - transitionings['E21'][0]
+        I12_new = I12 + transitionings['E12'][0] - transitionings['I12'][0]
+        I21_new = I21 + transitionings['E21'][0] - transitionings['I21'][0]        
+        R_new = R + transitionings['I12'][0] + transitionings['I21'][0]
+        
+        return S_new, S1_new, S2_new, E1_new, E2_new, E12_new, E21_new, I1_new, I2_new, I12_new,I21_new,  R1_new, R2_new,   R_new
+    
+
 class JumpProcess_SEIRH_BetaPerAge_SeasonalForcing(JumpProcess):
     """
     Stochastic SEIR2 model for DENV with 2 serotypes, baseline beta values per age-group and seasonal forcing.
@@ -260,8 +334,6 @@ class JumpProcess_SEIR2_BetaPerAge_SeasonalForcing(JumpProcess):
         R_new = R + transitionings['I12'][0] + transitionings['I21'][0]
         
         return S_new, S1_new, S2_new, E1_new, E2_new, E12_new, E21_new, I1_new, I2_new, I12_new,I21_new,  R1_new, R2_new,   R_new
-    
-
 
 
     ################################
@@ -533,9 +605,9 @@ class JumpProcess_SEIR2_spatial_age_sto_einstein(JumpProcess):
     """
     
     states = ['S','S1', 'S2', 'E1', 'E2', 'E12', 'E21', 'I1', 'I2', 'I12', 'I21', 'R1', 'R2', 'R', 'I_cum']
-    parameters = ['alpha', 'beta_1', 'sigma', 'gamma', 'psi', 'ph', 'ODmatrix', 'f_h', 'N'] # the beta_1 parameter is the same over all age-groups seeing as seasonality affects everyone the same
-    stratified_parameters = [[],['beta_0']] ## beta_0 is stratified per age group
-    dimensions = ['NIS','age_group']
+    parameters = ['alpha', 'beta_1', 'sigma', 'gamma', 'psi', 'ph', 'f_h', 'beta_0', 'ODmatrix', 'N'] # the beta_1 parameter is the same over all age-groups seeing as seasonality affects everyone the same
+    # stratified_parameters = [['beta_0', 'ODmatrix'], ['N']] ## beta_0 is stratified per age group, along with ODmatrix. N is stratified by location. The parameter in the stratified_parameters don't need to be mentioned in parameters = 
+    dimensions = ['age_group','location']
 
     @staticmethod
     def compute_rates(t, S,S1, S2, E1, E2, E12, E21, I1, I2, I12, I21, R1, R2, R, I_cum, #time + SEIR2 states
@@ -546,13 +618,14 @@ class JumpProcess_SEIR2_spatial_age_sto_einstein(JumpProcess):
         #beta_t = beta_0 * (1 - beta_1 * np.cos(2*np.pi *(t/365) + ph)) # this should result in 4 values per timepoint (one for each age-group)
         beta_t = beta_0 * (1 - beta_1 * np.sin(2*np.pi *(t/365) + ph))
 
+        print("ODmatrix", ODmatrix)
         #################################
         # Calculate total population
         #################################
 
         T = S+E1+I1+R1+S1+E12+I12+E2+I2+R2+S2+E21+I21+R # this should result in a total population per location, haven't checked this though
 
-        # for the total population and for the relevant compartments I1, I2, I12, I21
+        # # for the total population and for the relevant compartments I1, I2, I12, I21
         NS = S.shape[0] #age stratification (number of agegroups) 
         NA = S.shape[1] #spatial stratification (number of shapes)
 
@@ -565,37 +638,32 @@ class JumpProcess_SEIR2_spatial_age_sto_einstein(JumpProcess):
         # - 'a' is the age group,
         # - 'j' is the departure location we sum over,
         # - 'k' is the destination location.
-        I1_mob = np.einsum('ajk,aj->ak', ODmatrix, I1) # resulting is ages x destination location
-        I2_mob = np.einsum('ajk,aj->ak', ODmatrix, I2)
-        I12_mob = np.einsum('ajk,aj->ak', ODmatrix, I12)
-        I21_mob = np.einsum('ajk,aj->ak', ODmatrix, I21)
-        T_mob = np.einsum('ajk,aj->ak', ODmatrix, T)
+        I1_mob = np.einsum('jla,aj->al', ODmatrix, I1) # resulting is ages x destination location
+        I2_mob = np.einsum('jla,aj->al', ODmatrix, I2)
+        I12_mob = np.einsum('jla,aj->al', ODmatrix, I12)
+        I21_mob = np.einsum('jla,aj->al', ODmatrix, I21)
+        T_mob = np.einsum('jla,aj->al', ODmatrix, T)
 
         #################################
         # COMPUTE Force of Infection #### This is written to have different OD matrix per age-group
         #################################
 
         # Perform the multiplication: summing over the age groups from the infectious - result is number of interactions with susceptible age groups x locations
-
-        # OD: age x departures x arrivals
-        # I_mob : age x locations
-        # N : locations x ages x ages 
-
         # 'kj,ikj->ij' means:
         # - 'a' is the age group (infectious) we sum over,
         # - 'i' is the age group (susceptibles) 
-        # - 'j' is the destination location 
-        lambda_home_sero1 = beta_t * (f_h) * np.einsum('aj,iaj->ij', (I1+ psi*I21)/T, N) 
-        lambda_home_sero2 = beta_t * (f_h) * np.einsum('aj,iaj->ij', (I2+ psi*I12)/T, N)
+        # - 'l' is the destination location 
+        lambda_home_sero1 = beta_t[:, np.newaxis] * (f_h) * np.einsum('il,ail->al', (I1+ psi*I21)/T, N)  ## we need the [:, np.newaxis] because otherwise we are multiplying a beta_1 size (2,) with (2,3) 
+        lambda_home_sero2 = beta_t[:, np.newaxis]  * (f_h) * np.einsum('il,ail-> al', (I2+ psi*I12)/T, N)
 
         # Perform the multiplication: summing over the age groups from the infectious and the departure locations - result is number of interactions with suscpetible age groups x locations - 
         # 'kj,ikj->ij' means:
-        # - 'a' is the age group (infectious)
-        # - 'i' is the age group (susceptibles) we sum over,
-        # - 'j' is the destination location 
-        # - 'h' is departure locations we sum over,
-        lambda_visit_sero1 = beta_t * (1-f_h) * np.einsum ('ahj,aj,iaj->ij' , ODmatrix , (I1_mob + psi*I21_mob)/T_mob , N)
-        lambda_visit_sero2 = beta_t * (1-f_h) * np.einsum ('ahj,aj,iaj->ij' , ODmatrix , (I2_mob + psi*I12_mob)/T_mob , N)
+        # - 'a' is the age group (susceptibles)
+        # - 'i' is the age group (infectious) we sum over
+        # - 'j' is the origin location 
+        # - 'l' is destination locations we sum over,
+        lambda_visit_sero1 = beta_t[:, np.newaxis] * (1-f_h) * np.einsum ('jla, il, ail -> aj' , ODmatrix , (I1_mob + psi*I21_mob)/T_mob , N)
+        lambda_visit_sero2 = beta_t[:, np.newaxis] * (1-f_h) * np.einsum ('jla, il, ail -> aj' , ODmatrix , (I2_mob + psi*I12_mob)/T_mob , N)
 
 
         #################################
@@ -626,9 +694,9 @@ class JumpProcess_SEIR2_spatial_age_sto_einstein(JumpProcess):
         return rates
     
     @staticmethod
-    def apply_transitionings(t, tau, transitionings, S,S1, S2, E1, E2, E12, E21, I1, I2, I12, I21, R1, R2, R, I_cum,  #time + SEIR2 states
+    def apply_transitionings(t, tau, transitionings, S,S1, S2, E1, E2, E12, E21, I1, I2, I12, I21, R1, R2, R, I_cum, #time + SEIR2 states
                       beta_1, alpha, sigma, gamma, psi, ph, #SEIR2 parameters
-                      beta_0, ODmatrix): # age-stratified parameter
+                      beta_0, ODmatrix, f_h, N): # 
         
         S_new  = S - transitionings['S'][0] - transitionings['S'][1]
         E1_new = E1 + transitionings['S'][0] - transitionings['E1'][0]
