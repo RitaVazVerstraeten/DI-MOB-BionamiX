55b2e57bf7a09d18cceca34fbc80f88b4dc00d9f
Rita Verstraeten <rita.verstraeten@ugent.be>
2025-02-27 18:28:44 +0100

    testing Calibration Lambrechts with updated parameters and bounds

--- src/DENV_model/Calibration_Lamb_05022025.py
+++ src/DENV_model/Calibration_Lamb_05022025.py
@@ -14,8 +14,8 @@ import matplotlib.pyplot as plt
 # pySODM packages
 from pySODM.optimization import pso, nelder_mead
 from pySODM.optimization.utils import add_negative_binomial_noise, assign_theta, variance_analysis
-from pySODM.optimization.mcmc import perturbate_theta, run_EnsembleSampler, emcee_sampler_to_dictionary
-from pySODM.optimization.objective_functions import log_posterior_probability, ll_negative_binomial, ll_poisson
+from pySODM.optimization.mcmc import perturbate_theta, run_EnsembleSampler
+from pySODM.optimization.objective_functions import log_posterior_probability, ll_negative_binomial
 
 # # OPTIONAL: Load the "autoreload" extension so that package code can change
 # %load_ext autoreload
@@ -29,24 +29,26 @@ from pySODM.optimization.objective_functions import log_posterior_probability, l
 ##############
 import multiprocessing as mp
 
-tau = 7.0                                        # Timestep of Tau-Leaping algorithm
-# alpha = 0.03                                    # Overdispersion factor (based on COVID-19)
+tau = 1.0                                        # Timestep of Tau-Leaping algorithm
+# output_timestep = 7                             # only store the output of the model simulations every 7 days (per week)
+time_unit = "W"
+# alpha = 0.03                                   # Overdispersion factor (based on COVID-19)
 start_calibration = '2012-06-01'                 # start_date of calibration
-n_pso = 30                                      # Number of PSO iterations
+n_pso = 2                                     # Number of PSO iterations
 multiplier_pso = 10                             # PSO swarm size
-n_mcmc = 500                                    # Number of MCMC iterations
+n_mcmc = 5                                   # Number of MCMC iterations
 multiplier_mcmc = 10                            # Total number of Markov chains = number of parameters * multiplier_mcmc
 print_n = 100                                   # Print diagnostics every print_n iterations
 discard = 50                                    # Discard first `discard` iterations as burn-in
 thin = 10                                       # Thinning factor emcee chains
 n = 100                                         # Repeated simulations used in visualisations
 # processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count())) # Retrieve CPU count  
-processes = 3                                   # 3 so that if I run all 4 scaling factor scripts simultaneously, I can use my 12 cores. 
+processes = 6                                   # 3 so that if I run all 4 scaling factor scripts simultaneously, I can use my 12 cores. 
 
 # Variables
 samples_path='optimization_Lamb/sampler_output_Lamb/'
 fig_path='optimization_Lamb/sampler_output_Lamb/'
-identifier = 'Lambrechts_rverstra_' # Give any output of this script an ID
+identifier = 'Lambrechts_rverstra' # Give any output of this script an ID
 run_date = str(datetime.date.today())
 
 ####################################
@@ -72,14 +74,19 @@ start_date = pd.to_datetime(start_calibration)
 end_date = counts.index[-1]
 counts = counts[start_date:end_date]
 
+########## TURN COUNTS INTO A WEEKLY DATASET CUMMULATED ON SATURDAYS #######
+counts_weekly = counts.resample('W-SAT').sum()
+# reset start_date and end_date to match weekly dates: 
+start_date = counts_weekly.index.min()
+
 if __name__ == '__main__':    
     #############################################################
     # check mean-variance relation to choose likelihood function
     #############################################################
 
-    results, ax = variance_analysis(counts, resample_frequency='W')
+    results, ax = variance_analysis(counts_weekly, resample_frequency='W')
     alpha = results.loc['negative binomial', 'theta']
-    #print(results)
+    print(results)
     plt.show()
     plt.close()
 
@@ -100,7 +107,7 @@ from Scaling_functions_beta_t import generate_scaling_factors
 
 # LOAD THE METEO DATA FOR SCALING FACTORS
 # Paths and file
-file_path = "/media/rita/New Volume/Documenten/DI-MOB/Data Sharing/WP2_Meteorological_Data"
+file_path = "/home/rita/PyProjects/DI-MOB-BionamiX/data/WP2_Meteorological_Data"
 file = "Meteorologicas_2012_2022_withSE_v04102024_v1.xlsx"
 
 # Load and preprocess the meteorological data
@@ -134,23 +141,9 @@ scaling_factors, time = generate_scaling_factors(
 for key, df in scaling_factors.items():
     df.index = meteo.index
 
-###############################################
+# ##############################################
 # Get overlap between dates sf and epidemiology
-###############################################
-
-# Find the intersection of indices
-common_dates = counts.index.intersection(meteo.index)
-# Filter counts to only common dates
-counts_filtered = counts.loc[common_dates]
-# Filter each DataFrame in scaling_factors to only common dates
-# scaling_factors_filtered = {key: df.loc[common_dates] for key, df in scaling_factors.items()}
-
-# Sort counts_filtered by date and reset index
-counts_filtered = counts_filtered.sort_index()
-
-###############################################
-# Set-up model parameters & initial conditions
-###############################################
+# ##############################################
 
 demo = pd.read_excel("/home/rita/PyProjects/DI-MOB-BionamiX/data/WP1_ Demographic_Data/Population counts and proportions Municipio Cfgos_v17092024.xlsx")
 
@@ -162,10 +155,36 @@ scaling_factors_filtered = {
     for key, df in scaling_factors.items()
 }
 
+scaling_factors_weekly = {
+    model: df['sf'].resample('W-SAT').mean()  # Resample from Saturday to Saturday and take the mean
+    for model, df in scaling_factors_filtered.items()
+}
+
+# Find the intersection of indices
+common_dates = counts_weekly.index.intersection(scaling_factors_weekly['lambrechts'].index)
+counts_filtered = counts_weekly.loc[common_dates]
+# Filter each DataFrame in scaling_factors to only common dates
+scaling_factors_filtered = {key: df.loc[common_dates] for key, df in scaling_factors_weekly.items()}
+# reset the end_date to match the weekly structure and show the minimal date between counts and sf
+end_date = common_dates.max()
+
+###############################################
+# Set-up model parameters & initial conditions
+###############################################
+from Utilities import convert_params_to_weeks
+
 params={'alpha':182.5, 'b':2.77e-05, 'd':2.45e-05, 'sigma':6, 'gamma':7, 'psi': 1.5, 'beta_0' : 0.3, 'sf' : scaling_factors_filtered['lambrechts'], 'rho' : 0.10} 
 
+# convert daily rates into weekly rates
+if time_unit == "W": 
+    params_weekly = convert_params_to_weeks(params)
+    params = params_weekly
+
+# make sure that the indexer sf_t = self.sf.loc[t, 'sf'] in time_dependent_parameters would work: 
+params["sf"] = params["sf"].to_frame() # instead of a Series
+
 # zet initiele conditie gelijk aan 1/rho * I_reported en verdeel deze mensen over I1, I2, I12, en I21: 
-initial_infected = (1/params["rho"])*counts[start_date]
+initial_infected = (1/params["rho"])*counts_filtered[start_date]
 
 # Equal probabilities for 4 infectious compartments
 probabilities = [0.25, 0.25, 0.25, 0.25]
@@ -201,7 +220,7 @@ model = SEIR2_v2(initial_states=initial_states,
 
 if __name__ == '__main__':
 
-    a = 0.885382
+    a =  0.440015
     # Define dataset
     counts_filtered.index.name = 'date'
     d = pd.Series(counts_filtered)
@@ -212,11 +231,18 @@ if __name__ == '__main__':
     log_likelihood_fnc = [ll_negative_binomial,]
     log_likelihood_fnc_args = [a,]
     # Calibated parameters and bounds
-    pars = ['alpha', 'beta_0', 'sigma','rho'] #TCI, baseline infectioun rate, IIP, and fraction of reported cases 
-    labels = ['$alpha$' ,'$beta_0$', '$sigma$', '$rho$']
-    bounds = [(0.01, 720), (0.1, 0.4), (4.00, 7.00), (0, 1)]
+    pars = ['alpha', 'beta_0', 'sigma', 'gamma', 'rho'] #TCI, baseline infectioun rate, IIP, and fraction of reported cases 
+    labels = ['$\\alpha$' ,'$\\beta_0$', '$\\sigma$', '$\\gamma$','$\\rho$']
+    bounds = [(0.01, 720), (0.1, 0.4), (4.00, 10.00),(4.00, 10.00), (0, 1)]
 
-    objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,labels=labels)
+    # objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,labels=labels)
+    objective_function = log_posterior_probability(model,pars,bounds,data,states,
+                                                    log_likelihood_fnc,log_likelihood_fnc_args,
+                                                    simulation_kwargs={'tau':tau, 'time_unit': time_unit}, # 
+                                                    labels=labels)
+    # Extract expanded bounds and labels
+    expanded_labels = objective_function.expanded_labels 
+    expanded_bounds = objective_function.expanded_bounds 
 
     ####################
     # PSO / Nelder-Mead
@@ -225,23 +251,21 @@ if __name__ == '__main__':
     from Utilities import pso_to_dictionary, nelder_mead_to_dictionary
 
     # Initial guess --> pso
-    g, fg = pso.optimize(objective_function, swarmsize=3*18, max_iter=n_pso, processes=processes, debug=True)
-    theta = g
-    # store the pso results
-    pso_results = pso_to_dictionary(samples_path, identifier, run_date, g, fg)
+    theta, _ = pso.optimize(objective_function, swarmsize=multiplier_pso*processes, max_iter=n_pso, processes=processes, debug=True)
+    # # store the pso results
+    # pso_results = pso_to_dictionary(samples_path, identifier, run_date, g, fg)
 
-    # run nelder_mead
-    theta, ftheta = nelder_mead.optimize(objective_function, theta, 0.10*np.ones(len(theta)), processes=processes, max_iter=n_pso)
-    # store the nelder_mead results
-    nelder_mead_results = nelder_mead_to_dictionary(samples_path, identifier, run_date, theta, ftheta, history=None)
+    # Nelder-mead
+    step = len(expanded_bounds)*[0.10,]
+    theta, _ = nelder_mead.optimize(objective_function, np.array(theta), step, processes=processes,
+    max_iter=n_pso)
+
+    # # store the nelder_mead results
+    # nelder_mead_results = nelder_mead_to_dictionary(samples_path, identifier, run_date, theta, ftheta, history=None)
     
     ##########
     ## MCMC ##
     ##########
-
-    # Extract expanded bounds and labels
-    expanded_labels = objective_function.expanded_labels 
-    expanded_bounds = objective_function.expanded_bounds  
 
     # Perturbate previously obtained estimate
     ndim, nwalkers, pos = perturbate_theta(theta, pert=0.30*np.ones(len(theta)), multiplier=multiplier_mcmc, bounds=expanded_bounds)
@@ -251,19 +275,22 @@ if __name__ == '__main__':
     start_calibration = start_date.strftime("%Y-%m-%d")
     end_calibration = end_date.strftime("%Y-%m-%d")
 
-    settings={'start_calibration': start_calibration, 'end_calibration': end_calibration,'n_chains': nwalkers, 'starting_estimate': list(theta), 'labels': expanded_labels, 'tau': tau}
+
+    # CHECKING WHAT STARTING_ESTIMATES ARE GIVING ME ISSUES ...
+    starting_estimate = list(theta)
+    # Convert each element of the list to a native Python float
+    starting_estimate = [float(x) for x in theta]
+
+    settings={'start_calibration': start_calibration, 'end_calibration': end_calibration, 'starting_estimate': starting_estimate, 'tau': tau}
 
     # Sample n_mcmc iterations
-    sampler = run_EnsembleSampler(pos, n_mcmc, identifier,
+    sampler, samples_xr = run_EnsembleSampler(pos, n_mcmc, identifier,
                                 objective_function,
-                                objective_function_kwargs={'simulation_kwargs': {'tau':tau}},
-                                fig_path=fig_path, samples_path=samples_path, print_n=print_n, backend=None, processes=processes, progress=True,
-                                settings_dict=settings)       
+                                objective_function_kwargs= None, #,
+                                fig_path=fig_path, samples_path=samples_path, print_n=print_n, backend=None, processes=processes, progress=True
+                                , settings_dict=settings)       
 
 
-    # Generate a sample dictionary and save it as .json for long-term storage
-    # Have a look at the script `emcee_sampler_to_dictionary.py`, which does the same thing as the function below but can be used while your MCMC is running.
-    samples_dict = emcee_sampler_to_dictionary(samples_path, identifier, discard=discard, thin=thin)
     # Look at the resulting distributions in a cornerplot
     CORNER_KWARGS = dict(smooth=0.90,title_fmt=".2E")
     fig = corner.corner(sampler.get_chain(discard=discard, thin=2, flat=True), labels=expanded_labels, **CORNER_KWARGS)
