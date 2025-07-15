4e958c34164d4ee31c95ba1dbc2be458d5e8936d
Rita Verstraeten <rita.verstraeten@ugent.be>
2025-07-15 15:26:25 +0200

    updated reporting rate to baseline rate (secondary infection) and relative detection probability of prim/ter/quat infection relative to a secondary infection

--- src/DENV_model/model_4sero.py
+++ src/DENV_model/model_4sero.py
@@ -28,7 +28,8 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
     sigma : incubation period
     gamma : infectious period
     psi : enhanced / inhibited infectiousness of secondary infection
-    rho:  percentage; reported cases / all infected (constant over time)
+    rho:  proportion; reported cases / all infected (for secondary infections) (based on Imai et al. (2016))
+    delta:  proportion; probability of detecting a primary/tertiary infection relative to a secondary infection (based on Imai et al. (2016))
     ---------------
 
     The temperature depedence is implemented through an equations that represents the temperature-dependence of mosquito variables. These equations have also been published in Andronico et al. (2024)
@@ -90,7 +91,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
 
     states = ['S', 'S1', 'S2', 'S3', 'S4', 'S12', 'S13', 'S14', 'S21', 'S23', 'S24', 'S31', 'S32', 'S34', 'S41', 'S42', 'S43', 'S123', 'S124', 'S132', 'S134', 'S142', 'S143', 'S213', 'S214', 'S231', 'S234', 'S241', 'S243', 'S312', 'S314', 'S321', 'S324', 'S341', 'S342', 'S412', 'S413', 'S421', 'S423', 'S431', 'S432', 'E1', 'E2', 'E3', 'E4', 'E12', 'E13', 'E14', 'E21', 'E23', 'E24', 'E31', 'E32', 'E34', 'E41', 'E42', 'E43', 'E123', 'E124', 'E132', 'E134', 'E142', 'E143', 'E213', 'E214', 'E231', 'E234', 'E241', 'E243', 'E312', 'E314', 'E321', 'E324', 'E341', 'E342', 'E412', 'E413', 'E421', 'E423', 'E431', 'E432', 'E1234', 'E1243', 'E1324', 'E1342', 'E1423', 'E1432', 'E2134', 'E2143', 'E2314', 'E2341', 'E2413', 'E2431', 'E3124', 'E3142', 'E3214', 'E3241', 'E3412', 'E3421', 'E4123', 'E4132', 'E4213', 'E4231', 'E4312', 'E4321', 'I1', 'I2', 'I3', 'I4', 'I12', 'I13', 'I14', 'I21', 'I23', 'I24', 'I31', 'I32', 'I34', 'I41', 'I42', 'I43', 'I123', 'I124', 'I132', 'I134', 'I142', 'I143', 'I213', 'I214', 'I231', 'I234', 'I241', 'I243', 'I312', 'I314', 'I321', 'I324', 'I341', 'I342', 'I412', 'I413', 'I421', 'I423', 'I431', 'I432', 'I1234', 'I1243', 'I1324', 'I1342', 'I1423', 'I1432', 'I2134', 'I2143', 'I2314', 'I2341', 'I2413', 'I2431', 'I3124', 'I3142', 'I3214', 'I3241', 'I3412', 'I3421', 'I4123', 'I4132', 'I4213', 'I4231', 'I4312', 'I4321', 'R1', 'R2', 'R3', 'R4', 'R12', 'R13', 'R14', 'R21', 'R23', 'R24', 'R31', 'R32', 'R34', 'R41', 'R42', 'R43', 'R123', 'R124', 'R132', 'R134', 'R142', 'R143', 'R213', 'R214', 'R231', 'R234', 'R241', 'R243', 'R312', 'R314', 'R321', 'R324', 'R341', 'R342', 'R412', 'R413', 'R421', 'R423', 'R431', 'R432', 'R', 'I_new', 'I_cum', 'I_rep', 'I_1_rep', 'I_2_rep', 'I_3_rep', 'I_4_rep']
     
-    parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'sf'] 
+    parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'delta', 'sf'] 
 
     ##########################################    
     # parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'sf'] 
@@ -107,7 +108,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
 
                       I_new , I_cum , I_rep , I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                       
-                      b, d, beta_0, alpha, sigma, gamma, psi,rho, sf):
+                      b, d, beta_0, alpha, sigma, gamma, psi,rho, delta, sf):
         
         """
         Compute the rates for each state in the SEIR4 model with 4 serotypes.
@@ -124,7 +125,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             R1 , R2 , R3 , R4 , R12 , R13 , R14 , R21 , R23 , R24 , R31 , R32 , R34 , R41 , R42 , R43 , R123 , R124 , R132 , R134 , R142 , R143 , R213 , R214 , R231 , R234 , R241 , R243 , R312 , R314 , R321 , R324 , R341 , R342 , R412 , R413 , R421 , R423 , R431 , R432 , R , 
             I_new , I_cum , I_rep , I_1_rep, I_2_rep, I_3_rep, I_4_rep
         model parameters:
-        b, d, beta_0, alpha, sigma, gamma, psi,rho, sf
+        b, d, beta_0, alpha, sigma, gamma, psi,rho, delta, sf
         
         Returns
         -------
@@ -263,7 +264,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
 
                             I_new , I_cum , I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                             
-                            b, d, beta_0, alpha, sigma, gamma, psi, rho, sf):
+                            b, d, beta_0, alpha, sigma, gamma, psi, rho, delta, sf):
         
         """
         Apply the transitionings to the current states and return the new states.
@@ -287,7 +288,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             I_new , I_cum , I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep
             
         model parameters:
-            b, d, beta_0, alpha, sigma, gamma, psi, rho, sf
+            b, d, beta_0, alpha, sigma, gamma, psi, rho, delta, sf
             
         Returns
         -------                                                                          The individual states after applying the transitionings.
@@ -397,7 +398,17 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                 
             elif state == "I_rep":
                 # I_rep is rho * I_new -> reported cases at timestep t
-                new_state_array[idx] = rho * new_state_array[state_name_to_idx["I_new"]]
+                transitions = 0
+                for s in states_list:
+                    if s.startswith("E"):
+                        # The corresponding I state is "I" + s[1:]
+                        I_state = "I" + s[1:]
+                        # Secondary infection: length 3 (e.g., I21, I32)
+                        if len(I_state) == 3:
+                            transitions += rho * transitionings[s][0]
+                        else:
+                            transitions += rho* delta * transitionings[s][0]
+                new_state_array[idx] = transitions
             
             # get the number of reported cases per serotype (I_1_rep, I_2_rep, etc.)
             elif state.startswith("I_") and state.endswith("_rep"):
@@ -405,9 +416,16 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                 sero = state[2]  # '1', '2', '3', or '4'
                 # Find all E-states ending with this serotype
                 E_states = [s for s in states_list if s.startswith("E") and s[-1] == sero]
-                # Sum all transitions from these E-states to I
-                transitions = sum(transitionings[s][0] for s in E_states)
-                new_state_array[idx] = rho * transitions
+                transitions = 0
+                for E_state in E_states:
+                    # The corresponding I state is "I" + E_state[1:]
+                    I_state = "I" + E_state[1:]
+                    # Determine if this is a secondary infection (length 3, e.g. I21, I32)
+                    if len(I_state) == 3: # for secondary infections
+                        transitions += rho * transitionings[E_state][0]
+                    else: # for all primary tertiary and quaternary infections
+                        transitions += rho* delta * transitionings[E_state][0]
+                new_state_array[idx] = transitions
                 
             elif state == "I_cum":
                 # I_cum is previous I_cum plus I_new
