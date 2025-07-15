f5ddeefd40c24a0c0f2b3c84ab246a573434bca9
Rita Verstraeten <rita.verstraeten@ugent.be>
2025-07-15 09:26:17 +0200

    I_rep split into I_x_rep per serotype

--- src/DENV_model/model_4sero.py
+++ src/DENV_model/model_4sero.py
@@ -73,10 +73,13 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
     
     # append the "special" states for new infections, cumulative infections and reported infections
     states_list.extend(['I_new', 'I_cum', 'I_rep'])
+    
+    # add an extra I_rep per serotype
+    states_list.extend(['I_1_rep', 'I_2_rep', 'I_3_rep', 'I_4_rep'])
 
     # LENGTH OF THE STATES LIST
     # print("Length of states_list:", len(states_list))
-    print(states_list)
+    # print(states_list)
 
     ####################################
     # Create a mapping from state names to indices
@@ -85,7 +88,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
     # This will be used to quickly access the index of a state in the states_list
     state_name_to_idx = {state: i for i, state in enumerate(states_list)}
 
-    states = ['S', 'S1', 'S2', 'S3', 'S4', 'S12', 'S13', 'S14', 'S21', 'S23', 'S24', 'S31', 'S32', 'S34', 'S41', 'S42', 'S43', 'S123', 'S124', 'S132', 'S134', 'S142', 'S143', 'S213', 'S214', 'S231', 'S234', 'S241', 'S243', 'S312', 'S314', 'S321', 'S324', 'S341', 'S342', 'S412', 'S413', 'S421', 'S423', 'S431', 'S432', 'E1', 'E2', 'E3', 'E4', 'E12', 'E13', 'E14', 'E21', 'E23', 'E24', 'E31', 'E32', 'E34', 'E41', 'E42', 'E43', 'E123', 'E124', 'E132', 'E134', 'E142', 'E143', 'E213', 'E214', 'E231', 'E234', 'E241', 'E243', 'E312', 'E314', 'E321', 'E324', 'E341', 'E342', 'E412', 'E413', 'E421', 'E423', 'E431', 'E432', 'E1234', 'E1243', 'E1324', 'E1342', 'E1423', 'E1432', 'E2134', 'E2143', 'E2314', 'E2341', 'E2413', 'E2431', 'E3124', 'E3142', 'E3214', 'E3241', 'E3412', 'E3421', 'E4123', 'E4132', 'E4213', 'E4231', 'E4312', 'E4321', 'I1', 'I2', 'I3', 'I4', 'I12', 'I13', 'I14', 'I21', 'I23', 'I24', 'I31', 'I32', 'I34', 'I41', 'I42', 'I43', 'I123', 'I124', 'I132', 'I134', 'I142', 'I143', 'I213', 'I214', 'I231', 'I234', 'I241', 'I243', 'I312', 'I314', 'I321', 'I324', 'I341', 'I342', 'I412', 'I413', 'I421', 'I423', 'I431', 'I432', 'I1234', 'I1243', 'I1324', 'I1342', 'I1423', 'I1432', 'I2134', 'I2143', 'I2314', 'I2341', 'I2413', 'I2431', 'I3124', 'I3142', 'I3214', 'I3241', 'I3412', 'I3421', 'I4123', 'I4132', 'I4213', 'I4231', 'I4312', 'I4321', 'R1', 'R2', 'R3', 'R4', 'R12', 'R13', 'R14', 'R21', 'R23', 'R24', 'R31', 'R32', 'R34', 'R41', 'R42', 'R43', 'R123', 'R124', 'R132', 'R134', 'R142', 'R143', 'R213', 'R214', 'R231', 'R234', 'R241', 'R243', 'R312', 'R314', 'R321', 'R324', 'R341', 'R342', 'R412', 'R413', 'R421', 'R423', 'R431', 'R432', 'R', 'I_new', 'I_cum', 'I_rep']
+    states = ['S', 'S1', 'S2', 'S3', 'S4', 'S12', 'S13', 'S14', 'S21', 'S23', 'S24', 'S31', 'S32', 'S34', 'S41', 'S42', 'S43', 'S123', 'S124', 'S132', 'S134', 'S142', 'S143', 'S213', 'S214', 'S231', 'S234', 'S241', 'S243', 'S312', 'S314', 'S321', 'S324', 'S341', 'S342', 'S412', 'S413', 'S421', 'S423', 'S431', 'S432', 'E1', 'E2', 'E3', 'E4', 'E12', 'E13', 'E14', 'E21', 'E23', 'E24', 'E31', 'E32', 'E34', 'E41', 'E42', 'E43', 'E123', 'E124', 'E132', 'E134', 'E142', 'E143', 'E213', 'E214', 'E231', 'E234', 'E241', 'E243', 'E312', 'E314', 'E321', 'E324', 'E341', 'E342', 'E412', 'E413', 'E421', 'E423', 'E431', 'E432', 'E1234', 'E1243', 'E1324', 'E1342', 'E1423', 'E1432', 'E2134', 'E2143', 'E2314', 'E2341', 'E2413', 'E2431', 'E3124', 'E3142', 'E3214', 'E3241', 'E3412', 'E3421', 'E4123', 'E4132', 'E4213', 'E4231', 'E4312', 'E4321', 'I1', 'I2', 'I3', 'I4', 'I12', 'I13', 'I14', 'I21', 'I23', 'I24', 'I31', 'I32', 'I34', 'I41', 'I42', 'I43', 'I123', 'I124', 'I132', 'I134', 'I142', 'I143', 'I213', 'I214', 'I231', 'I234', 'I241', 'I243', 'I312', 'I314', 'I321', 'I324', 'I341', 'I342', 'I412', 'I413', 'I421', 'I423', 'I431', 'I432', 'I1234', 'I1243', 'I1324', 'I1342', 'I1423', 'I1432', 'I2134', 'I2143', 'I2314', 'I2341', 'I2413', 'I2431', 'I3124', 'I3142', 'I3214', 'I3241', 'I3412', 'I3421', 'I4123', 'I4132', 'I4213', 'I4231', 'I4312', 'I4321', 'R1', 'R2', 'R3', 'R4', 'R12', 'R13', 'R14', 'R21', 'R23', 'R24', 'R31', 'R32', 'R34', 'R41', 'R42', 'R43', 'R123', 'R124', 'R132', 'R134', 'R142', 'R143', 'R213', 'R214', 'R231', 'R234', 'R241', 'R243', 'R312', 'R314', 'R321', 'R324', 'R341', 'R342', 'R412', 'R413', 'R421', 'R423', 'R431', 'R432', 'R', 'I_new', 'I_cum', 'I_rep', 'I_1_rep', 'I_2_rep', 'I_3_rep', 'I_4_rep']
     
     parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'sf'] 
 
@@ -102,7 +105,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                       
                       R1 , R2 , R3 , R4 , R12 , R13 , R14 , R21 , R23 , R24 , R31 , R32 , R34 , R41 , R42 , R43 , R123 , R124 , R132 , R134 , R142 , R143 , R213 , R214 , R231 , R234 , R241 , R243 , R312 , R314 , R321 , R324 , R341 , R342 , R412 , R413 , R421 , R423 , R431 , R432 , R , 
 
-                      I_new , I_cum , I_rep , 
+                      I_new , I_cum , I_rep , I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                       
                       b, d, beta_0, alpha, sigma, gamma, psi,rho, sf):
         
@@ -119,7 +122,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             E431, E432, E1234, E1243, E1324, E1342, E1423, E1432, E2134, E2143, E2314, E2341, E2413, E2431, E3124, E3142, E3214, E3241, E3412, E3421, E4123, E4132, E4213, E4231 E4312,  
             I1 , I2 , I3 , I4 , I12 , I13 , I14 , I21 , I23 , I24 , I31 , I32 , I34 , I41 , I42 , I43 , I123 , I124 , I132 , I134 , I142 , I143 , I213 , I214 , I231 , I234 , I241 , I243 , I312 , I314 , I321 , I324 , I341 , I342 , I412 , I413 , I421 , I423 , I431 , I432 , I1234 , I1243 , I1324 , I1342 , I1423 , I1432 , I2134 , I2143 , I2314 , I2341 , I2413 , I2431 , I3124 , I3142 , I3214 , I3241 , I3412 , I3421 , I4123 , I4132 , I4213 , I4231 , I4312 , I4321 , 
             R1 , R2 , R3 , R4 , R12 , R13 , R14 , R21 , R23 , R24 , R31 , R32 , R34 , R41 , R42 , R43 , R123 , R124 , R132 , R134 , R142 , R143 , R213 , R214 , R231 , R234 , R241 , R243 , R312 , R314 , R321 , R324 , R341 , R342 , R412 , R413 , R421 , R423 , R431 , R432 , R , 
-            I_new , I_cum , I_rep , 
+            I_new , I_cum , I_rep , I_1_rep, I_2_rep, I_3_rep, I_4_rep
         model parameters:
         b, d, beta_0, alpha, sigma, gamma, psi,rho, sf
         
@@ -153,14 +156,14 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             I1, I2, I3, I4, I12, I13, I14, I21, I23, I24, I31, I32, I34, I41, I42, I43, I123, I124, I132, I134, I142, I143, I213, I214, I231, I234, I241, I243, I312, I314, I321, I324, I341, I342, I412, I413, I421, I423, I431, I432, I1234, I1243, I1324, I1342, I1423, I1432, I2134, I2143, I2314, I2341, I2413, I2431, I3124, I3142, I3214, I3241, I3412, I3421, I4123, I4132, I4213, I4231, I4312, I4321,
             
             R1, R2, R3, R4, R12, R13, R14, R21, R23, R24, R31, R32, R34, R41, R42, R43, R123, R124, R132, R134, R142, R143, R213, R214, R231, R234, R241, R243, R312, R314, R321, R324, R341, R342, R412, R413, R421, R423, R431, R432, R,
-            I_new, I_cum, I_rep]
+            I_new, I_cum, I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep]
 
         # use np array for each state value
         # Reconstruct the state array from the individual arguments
         state_array = np.array(state_values)    
         
         # Calculate total population as the sum of all states except the special states 
-        exclude_states = {'I_new', 'I_cum', 'I_rep'}
+        exclude_states = {'I_new', 'I_cum', 'I_rep', 'I_1_rep', 'I_2_rep', 'I_3_rep', 'I_4_rep'}
         exclude_idxs = [state_name_to_idx[s] for s in exclude_states if s in state_name_to_idx]
 
         T = np.sum(np.delete(state_array, exclude_idxs))
@@ -221,7 +224,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                 (1 / sigma) * np.ones(state_array[idx].shape),
                 d * np.ones(state_array[idx].shape)]
                 
-            elif state.startswith("I") and state not in ["I_new", "I_cum", "I_rep"]:
+            elif state.startswith("I") and state not in ["I_new", "I_cum", "I_rep", "I_1_rep", "I_2_rep", "I_3_rep", "I_4_rep"]:
                 rates[state] = [
                     (1 / gamma) * np.ones(state_array[idx].shape),
                     d * np.ones(state_array[idx].shape)]
@@ -233,9 +236,20 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                         d * np.ones(state_array[idx].shape)]
                 else: # just "R"
                     rates[state] = [d * np.ones(state_array[idx].shape)] # death rate
-                    
+        # # TRYING TO RESOLVE ValueError: p < 0, p > 1 or p contains NaNs
+        # # After rates have been computed for all states
+        # for state, rate_list in rates.items():
+        #     for i, rate in enumerate(rate_list):
+        #         # Convert to numpy array if not already
+        #         arr = np.array(rate)
+        #         if np.any(arr < 0) or np.any(arr > 1) or np.any(np.isnan(arr)):
+        #             print(f"State: {state}, Rate index: {i}, Values: {arr[arr < 0] if np.any(arr < 0) else ''} {arr[arr > 1] if np.any(arr > 1) else ''} {'NaN' if np.any(np.isnan(arr)) else ''}")
+    
+    
         return rates
-            
+    
+
+           
                
     @staticmethod
     def apply_transitionings(t, tau, transitionings, 
@@ -247,7 +261,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                             
                             R1 , R2 , R3 , R4 , R12 , R13 , R14 , R21 , R23 , R24 , R31 , R32 , R34 , R41 , R42 , R43 , R123 , R124 , R132 , R134 , R142 , R143 , R213 , R214 , R231 , R234 , R241 , R243 , R312 , R314 , R321 , R324 , R341 , R342 , R412 , R413 , R421 , R423 , R431 , R432 , R , 
 
-                            I_new , I_cum , I_rep,  
+                            I_new , I_cum , I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                             
                             b, d, beta_0, alpha, sigma, gamma, psi, rho, sf):
         
@@ -270,7 +284,8 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             
             R1 , R2 , R3 , R4 , R12 , R13 , R14 , R21 , R23 , R24 , R31 , R32 , R34 , R41 , R42 , R43 , R123 , R124 , R132 , R134 , R142 , R143 , R213 , R214 , R231 , R234 , R241 , R243 , R312 , R314 , R321 , R324 , R341 , R342 , R412 , R413 , R421 , R423 , R431 , R432 , R , 
 
-            I_new , I_cum , I_rep,  
+            I_new , I_cum , I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep
+            
         model parameters:
             b, d, beta_0, alpha, sigma, gamma, psi, rho, sf
             
@@ -296,7 +311,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             E1, E2, E3, E4, E12, E13, E14, E21, E23, E24, E31, E32, E34, E41, E42, E43, E123, E124, E132, E134, E142, E143, E213, E214, E231, E234, E241, E243, E312, E314, E321, E324, E341, E342, E412, E413, E421, E423, E431, E432, E1234, E1243, E1324, E1342, E1423, E1432, E2134, E2143, E2314, E2341, E2413, E2431, E3124, E3142, E3214, E3241, E3412, E3421, E4123, E4132, E4213, E4231, E4312, E4321,
             I1, I2, I3, I4, I12, I13, I14, I21, I23, I24, I31, I32, I34, I41, I42, I43, I123, I124, I132, I134, I142, I143, I213, I214, I231, I234, I241, I243, I312, I314, I321, I324, I341, I342, I412, I413, I421, I423, I431, I432, I1234, I1243, I1324, I1342, I1423, I1432, I2134, I2143, I2314, I2341, I2413, I2431, I3124, I3142, I3214, I3241, I3412, I3421, I4123, I4132, I4213, I4231, I4312, I4321,
             R1, R2, R3, R4, R12, R13, R14, R21, R23, R24, R31, R32, R34, R41, R42, R43, R123, R124, R132, R134, R142, R143, R213, R214, R231, R234, R241, R243, R312, R314, R321, R324, R341, R342, R412, R413, R421, R423, R431, R432, R,
-            I_new, I_cum, I_rep
+            I_new, I_cum, I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep
         ]
         state_array = np.array(state_values)
 
@@ -345,7 +360,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                 )
 
         # infectious                    
-            elif state.startswith("I") and state not in ["I_new", "I_cum", "I_rep"]:
+            elif state.startswith("I") and state not in ["I_new", "I_cum", "I_rep", "I_1_rep", "I_2_rep", "I_3_rep", "I_4_rep"]:
                 prior_E = "E" + state[1:]
                 # For regular I states, use the transitionings formula
                 new_state_array[idx] = (
@@ -370,7 +385,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
                     # For the final R (just "R")
                     new_state_array[idx] = (
                         state_array[idx] # the prior number in R
-                        + sum(transitionings[s][0] for s in states_list if s.startswith("I") and len(s) > 4 and s not in ["I_new", "I_cum", "I_rep"]) # sum of all I states with 4 serotype exposure
+                        + sum(transitionings[s][0] for s in states_list if s.startswith("I") and len(s) > 4 and s not in ["I_new", "I_cum", "I_rep","I_1_rep", "I_2_rep", "I_3_rep", "I_4_rep"]) # sum of all I states with 4 serotype exposure
                         - transitionings[state][0] # deaths
                     )
                     
@@ -383,6 +398,16 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported(JumpProcess):
             elif state == "I_rep":
                 # I_rep is rho * I_new -> reported cases at timestep t
                 new_state_array[idx] = rho * new_state_array[state_name_to_idx["I_new"]]
+            
+            # get the number of reported cases per serotype (I_1_rep, I_2_rep, etc.)
+            elif state.startswith("I_") and state.endswith("_rep"):
+                # e.g., state == "I_1_rep"
+                sero = state[2]  # '1', '2', '3', or '4'
+                # Find all E-states ending with this serotype
+                E_states = [s for s in states_list if s.startswith("E") and s[-1] == sero]
+                # Sum all transitions from these E-states to I
+                transitions = sum(transitionings[s][0] for s in E_states)
+                new_state_array[idx] = rho * transitions
                 
             elif state == "I_cum":
                 # I_cum is previous I_cum plus I_new
