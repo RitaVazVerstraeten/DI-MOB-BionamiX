9bd16df56c53a0f1d89a5305e0608df4b38fa59a
Rita Verstraeten <rita.verstraeten@ugent.be>
2025-07-31 09:19:47 +0200

    integration of intial_condition_function parameters into model to use pySODM to calibrate initial immunity

--- src/DENV_model/model_4sero_delta_1_3_ICF.py
+++ src/DENV_model/model_4sero_delta_1_3_ICF.py
@@ -1,5 +1,7 @@
-# DENV model with 4 serotypes & ZIKV
-
+# DENV model with 4 serotypes
+#############################################################
+# DELTA_1_3 MODEL ADJUSTED FOR USE INITIAL_CONDITION_FUNCTION
+#############################################################
 import pandas as pd
 import numpy as np
 from pySODM.models.base import ODE, JumpProcess
@@ -14,7 +16,7 @@ from itertools import permutations
 # TEMPORAL MODEL + 4 SEROTYPES AND 4 POSSIBLE INFECTIONS IN SERIES
 ################################
 
-class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
+class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF(JumpProcess):
     """
     Stochastic SEIR4 model for DENV with 4 serotypes
     No inclusion of age. All births go into susceptible compartment. All deaths leave from every possible compartment (they are natural deaths, unrelated to DENV). A fraction of all infected is considered "reported". 
@@ -91,7 +93,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
 
     states = ['S', 'S1', 'S2', 'S3', 'S4', 'S12', 'S13', 'S14', 'S21', 'S23', 'S24', 'S31', 'S32', 'S34', 'S41', 'S42', 'S43', 'S123', 'S124', 'S132', 'S134', 'S142', 'S143', 'S213', 'S214', 'S231', 'S234', 'S241', 'S243', 'S312', 'S314', 'S321', 'S324', 'S341', 'S342', 'S412', 'S413', 'S421', 'S423', 'S431', 'S432', 'E1', 'E2', 'E3', 'E4', 'E12', 'E13', 'E14', 'E21', 'E23', 'E24', 'E31', 'E32', 'E34', 'E41', 'E42', 'E43', 'E123', 'E124', 'E132', 'E134', 'E142', 'E143', 'E213', 'E214', 'E231', 'E234', 'E241', 'E243', 'E312', 'E314', 'E321', 'E324', 'E341', 'E342', 'E412', 'E413', 'E421', 'E423', 'E431', 'E432', 'E1234', 'E1243', 'E1324', 'E1342', 'E1423', 'E1432', 'E2134', 'E2143', 'E2314', 'E2341', 'E2413', 'E2431', 'E3124', 'E3142', 'E3214', 'E3241', 'E3412', 'E3421', 'E4123', 'E4132', 'E4213', 'E4231', 'E4312', 'E4321', 'I1', 'I2', 'I3', 'I4', 'I12', 'I13', 'I14', 'I21', 'I23', 'I24', 'I31', 'I32', 'I34', 'I41', 'I42', 'I43', 'I123', 'I124', 'I132', 'I134', 'I142', 'I143', 'I213', 'I214', 'I231', 'I234', 'I241', 'I243', 'I312', 'I314', 'I321', 'I324', 'I341', 'I342', 'I412', 'I413', 'I421', 'I423', 'I431', 'I432', 'I1234', 'I1243', 'I1324', 'I1342', 'I1423', 'I1432', 'I2134', 'I2143', 'I2314', 'I2341', 'I2413', 'I2431', 'I3124', 'I3142', 'I3214', 'I3241', 'I3412', 'I3421', 'I4123', 'I4132', 'I4213', 'I4231', 'I4312', 'I4321', 'R1', 'R2', 'R3', 'R4', 'R12', 'R13', 'R14', 'R21', 'R23', 'R24', 'R31', 'R32', 'R34', 'R41', 'R42', 'R43', 'R123', 'R124', 'R132', 'R134', 'R142', 'R143', 'R213', 'R214', 'R231', 'R234', 'R241', 'R243', 'R312', 'R314', 'R321', 'R324', 'R341', 'R342', 'R412', 'R413', 'R421', 'R423', 'R431', 'R432', 'R', 'I_new', 'I_cum', 'I_rep', 'I_1_rep', 'I_2_rep', 'I_3_rep', 'I_4_rep']
     
-    parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'delta_1', 'delta_3', 'sf'] 
+    parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'delta_1', 'delta_3', 'sf', 'epi_uf', 'f_imm_1', 'f_imm_2', 'f_imm_3', 'f_imm_4', 'pop_uf', 'compartment_fractions', 'states_list']
 
     ##########################################    
     # parameters = ['alpha','b', 'd', 'beta_0', 'sigma', 'gamma', 'psi', 'rho', 'sf'] 
@@ -108,7 +110,8 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
 
                       I_new , I_cum , I_rep , I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                       
-                      b, d, beta_0, alpha, sigma, gamma, psi,rho, delta_1,delta_3, sf):
+                      b, d, beta_0, alpha, sigma, gamma, psi,rho, delta_1,delta_3, sf, 
+                      epi_uf, f_imm_1, f_imm_2, f_imm_3, f_imm_4, pop_uf, compartment_fractions, states_list):
         
         """
         Compute the rates for each state in the SEIR4 model with 4 serotypes.
@@ -138,15 +141,15 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
         # state_name_to_idx = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported.state_name_to_idx
         
         # infectious_states_by_sero = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported.infectious_states_by_sero
-        serotypes = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.serotypes
+        serotypes = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.serotypes
         
-        states_list = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.states_list
+        states_list = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.states_list
         
-        state_name_to_idx = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.state_name_to_idx
+        state_name_to_idx = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.state_name_to_idx
         
-        infectious_states_by_sero = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.infectious_states_by_sero
+        infectious_states_by_sero = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.infectious_states_by_sero
         
-        primary_idx_by_sero = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.primary_idx_by_sero
+        primary_idx_by_sero = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.primary_idx_by_sero
 
         # All state arguments in order (excluding t and parameters at the end)
         state_values = [
@@ -270,7 +273,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
 
                             I_new , I_cum , I_rep, I_1_rep, I_2_rep, I_3_rep, I_4_rep,
                             
-                            b, d, beta_0, alpha, sigma, gamma, psi, rho, delta_1, delta_3, sf):
+                            b, d, beta_0, alpha, sigma, gamma, psi, rho, delta_1, delta_3, sf, epi_uf, f_imm_1, f_imm_2, f_imm_3, f_imm_4, pop_uf, compartment_fractions, states_list):
         
         """
         Apply the transitionings to the current states and return the new states.
@@ -303,14 +306,16 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
         # print("\n t in apply_transitionings", t)
         # transitionings = {k: [int(np.round(v, 0)) for v in (v if isinstance(v, list) else [v])] for k, v in transitionings.items()} # to avoid that the states become non-integegers and therefore go below 0
         # transitionings = {k: [int(np.round(v, 0))] for k, v in transitionings.items()}
-        transitionings = {k: [np.round(arr).astype(int) for arr in v] for k, v in transitionings.items()}
+        
+        # clip negative values to zero
+        transitionings = {k: [max(0, int(np.round(arr))) for arr in v] for k, v in transitionings.items()}
 
         
-        states_list = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.states_list
+        states_list = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.states_list
         
-        state_name_to_idx = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.state_name_to_idx
+        state_name_to_idx = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.state_name_to_idx
         
-        serotypes = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.serotypes
+        serotypes = JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.serotypes
         
         # Build state_array from arguments (order must match states_list)
         state_values = [
@@ -348,13 +353,14 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                         - sum(transitionings[state][i] for i in range(5))  # S -> E transitions & deaths
                         + transitionings[state][5]  # + births
                     )
+                    new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
                 
                 else: # for S1, S2, S3, S4, S12, S13, etc.
                     prior_R = "R" + state[1:]
                     new_state_array[idx] = state_array[idx] 
                     + transitionings[prior_R][0] # R -> S transitions 
                     - sum(transitionings[state]) # S -> E transitions and deaths
-                    
+                    new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
 
         # Exposed                    
             elif state.startswith("E"):
@@ -365,6 +371,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                     - transitionings[state][0] # E -> I transition
                     - transitionings[state][1] # deaths
                 )
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
 
         # infectious                    
             elif state.startswith("I") and state not in ["I_new", "I_cum", "I_rep", "I_1_rep", "I_2_rep", "I_3_rep", "I_4_rep"]:
@@ -376,6 +383,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                     - transitionings[state][0]  # I -> R transition
                     - transitionings[state][1]  # deaths
                 )
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
                 
         # recovered                
             elif state.startswith("R"):
@@ -388,6 +396,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                         - transitionings[state][0] # R -> S
                         - transitionings[state][1] # deaths
                     )
+                    new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
                 else:
                     # For the final R (just "R")
                     new_state_array[idx] = (
@@ -395,11 +404,13 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                         + sum(transitionings[s][0] for s in states_list if s.startswith("I") and len(s) > 4 and s not in ["I_new", "I_cum", "I_rep","I_1_rep", "I_2_rep", "I_3_rep", "I_4_rep"]) # sum of all I states with 4 serotype exposure
                         - transitionings[state][0] # deaths
                     )
+                    new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
                     
         # Derivative states
             elif state == "I_new":
                 # I_new is the sum of all transitions from any E state to I at timestep t
                 new_state_array[idx] = sum(transitionings[s][0] for s in states_list if s.startswith("E"))
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
 
                 
             elif state == "I_rep":
@@ -418,6 +429,7 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                         else:
                             transitions += rho* delta_3 * transitionings[s][0]
                 new_state_array[idx] = transitions
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
             
             # get the number of reported cases per serotype (I_1_rep, I_2_rep, etc.)
             elif state.startswith("I_") and state.endswith("_rep"):
@@ -436,10 +448,13 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
                     else: # Tertiary and quaternary infections (e.g., I231, I1234)
                         transitions += rho * delta_3 * transitionings[E_state][0]
                 new_state_array[idx] = transitions
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
+
                 
             elif state == "I_cum":
                 # I_cum is previous I_cum plus I_new
                 new_state_array[idx] = state_array[state_name_to_idx["I_cum"]] + new_state_array[state_name_to_idx["I_new"]]
+                new_state_array[idx] = max(0, int(np.round(new_state_array[idx])))
 
         
         # return the new states as a tuple in the order of the original states_list
@@ -447,12 +462,12 @@ class JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3(JumpProcess):
 
 # Add these lines after the class definition
 
-JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.infectious_states_by_sero = {
-    sero: [i for i, state in enumerate(JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.states_list)
+JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.infectious_states_by_sero = {
+    sero: [i for i, state in enumerate(JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.states_list)
            if state.startswith("I") and state.endswith(sero)]
     for sero in ["1", "2", "3", "4"]
 }
-JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.primary_idx_by_sero = {
-    sero: JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3.state_name_to_idx[f"I{sero}"]
+JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.primary_idx_by_sero = {
+    sero: JumpProcess_SEIR4_beta_by_Temp_sf_BirthDeath_reported_d_1_3_ICF.state_name_to_idx[f"I{sero}"]
     for sero in ["1", "2", "3", "4"]
 }
