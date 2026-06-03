6b4c3354f025ff3deb5953a3b8b01c43a9f43380
Rita Verstraeten <rita.verstraeten@ugent.be>
2026-06-03 09:36:34 +0200

    debug

--- src/Entomo/Hierarch_StateSpace_Entomo_model.r
+++ src/Entomo/Hierarch_StateSpace_Entomo_model.r
@@ -170,7 +170,7 @@ predictor_spec <- if (isTRUE(cfg$use_dlnm)) {
   paste0("lag-", paste(cfg$lag_vars, collapse = "-"),
          "_unlag-", paste(cfg$unlagged_vars, collapse = "-"))
 }
-run_suffix <- paste0(date_suffix, "_VPD_lin_TRD_lin_NoResidRain")
+run_suffix <- paste0(date_suffix, "_VPD_ns_TRD_ns_RESID_ns")
 
 model_output_dir  <- file.path(cfg$output_dir, predictor_spec, model_spec)
 run_output_dir    <- file.path(model_output_dir, run_suffix)
