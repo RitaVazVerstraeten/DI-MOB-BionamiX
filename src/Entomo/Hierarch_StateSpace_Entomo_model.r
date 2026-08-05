e4ee671e0653cc3c749972e5eeb85046b2e2ca0e
Rita Verstraeten <rita.verstraeten@ugent.be>
2026-08-05 18:18:29 +0200

    test spi-6 with avg_temp in the model

--- src/Entomo/Hierarch_StateSpace_Entomo_model.r
+++ src/Entomo/Hierarch_StateSpace_Entomo_model.r
@@ -92,7 +92,7 @@ cfg <- list(
   response_start = "2016_01",
   n_blocks = NULL, # set NULL for all blocks/CMFs
 
-  lag_vars = c("SPI6", "avg_VPD", "precip_max_day_resid_on_tp"),
+  lag_vars = c("SPI6", "avg_VPD", "precip_max_day_resid_on_tp", "avg_temp"),
   # lag_vars = c("total_rainy_days", "avg_VPD"),
 
   max_lag = 6,
@@ -109,15 +109,15 @@ cfg <- list(
   unlagged_vars = c("HFP_urbanization",  "water_containers"),
 
   # numeric_vars = c("total_precip",  "avg_VPD", "precip_max_day_resid_on_tp", "water_containers", "HFP_urbanization", "mean_ndvi"),
-  numeric_vars = c("SPI6",  "avg_VPD", "precip_max_day_resid_on_tp", "water_containers", "HFP_urbanization"),
+  numeric_vars = c("SPI6",  "avg_VPD", "precip_max_day_resid_on_tp", "water_containers", "HFP_urbanization", "avg_temp"),
 
   # DLNM settings (only used when use_dlnm = TRUE)
   dlnm_vars   = c("SPI6",  "avg_VPD", "precip_max_day_resid_on_tp"),
   dlnm_argvar = list(
     SPI6                        = list(fun = "ns", df = 3),
     avg_VPD                     = list(fun = "ns", df = 3),
-    precip_max_day_resid_on_tp = list(fun = "ns", df = 3)
-    # temp_resid_on_tp            = list(fun = "ns", df = 3)
+    precip_max_day_resid_on_tp = list(fun = "ns", df = 3),
+    avg_temp            = list(fun = "ns", df = 3)
   ),
   dlnm_arglag = list(fun = "ns", df = 3),  # shared lag basis across all DLNM vars
 
@@ -135,11 +135,11 @@ cfg <- list(
   #     through a straight line. Defaults to 2 if omitted.
   # Set dlnm_ix_vars = NULL to run the base DLNM model without interactions.
 
-  dlnm_ix_vars = list(
-    list(continuous_var = "HFP_urbanization", dlnm_var = "SPI6", label = "spi6_x_HFP", continuous_df = 2)
-    # list(binary_var = "water_shortage", active_level = 1, dlnm_var = "total_precip", label = "tp_x_shortage"),
-    # list(continuous_var = "water_containers", dlnm_var = "SPI6", label = "spi6_x_wc", continuous_df = 2)
-  ),
+  # dlnm_ix_vars = list(
+  #   list(continuous_var = "HFP_urbanization", dlnm_var = "SPI6", label = "spi6_x_HFP", continuous_df = 2)
+  #   # list(binary_var = "water_shortage", active_level = 1, dlnm_var = "total_precip", label = "tp_x_shortage"),
+  #   # list(continuous_var = "water_containers", dlnm_var = "SPI6", label = "spi6_x_wc", continuous_df = 2)
+  # ),
   dlnm_ix_vars = NULL,
 
   # MCMC
